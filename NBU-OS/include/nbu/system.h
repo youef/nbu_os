6dbe1fa849c4dd2e2fcf8377cfffd04b757fb5e6
youef <61435984+youef@users.noreply.github.com>
2026-09-26 05:04:16 +0300

    feat: update NBU-OS system version

--- NBU-OS/include/nbu/system.h
+++ NBU-OS/include/nbu/system.h
@@ -1,10 +1,8 @@
 #ifndef NBU_SYSTEM_H
 #define NBU_SYSTEM_H
-
 #define NBU_SYSTEM_NAME "NBU-OS"
-#define NBU_SYSTEM_VERSION "0.3.0-alpha1"
+#define NBU_SYSTEM_VERSION "0.4.0-alpha1"
 #define NBU_EXEC_ABI "NBU-EXEC-1"
 #define NBU_VENDOR "Northern Borders University - Digital Transformation"
 #define NBU_DEVELOPER "Yusuf Alhazmi"
-
 #endif
